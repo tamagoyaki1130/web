e6fc3e665d42b99a0036c0cd6d6e7b4573dc4b16
nhkim06 <nhkim7788@gmail.com>
2024-06-04 02:54:42 +0900

    adasasd

--- chatGPT.py
+++ chatGPT.py
@@ -2,8 +2,7 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 
-from flask import Flask, session, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request
 
 #ChatGPT 사용 코드. -> AI(user_input)하면 답변 return 함. 
 _=load_dotenv(find_dotenv())
@@ -28,3 +27,23 @@ def AI(user_input):
     ]
     )
     return completion.choices[0].message.content
+
+
+#website에 값 받고 출력하기
+
+app = Flask(__name__)
+
+@app.route('/')
+
+@app.route('/bird_friend')
+def bird_friend():
+    return render_template('index.html')
+
+@app.route("/result", methods =['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    name = output["name"]
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
