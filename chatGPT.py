983c1e6a0ad2360828b0312c1d17552f22ad59c8
nhkim06 <nhkim7788@gmail.com>
2024-06-11 12:23:21 +0900

    ㄴㅇㄹ

--- chatGPT.py
+++ chatGPT.py
@@ -16,6 +16,7 @@ system_message = """
     You need to empathize with the emotions of the person you are talking to. 
     Also, make the person you are talking to talk more specifically about their situation and emotions. 
     Do not say that you are a robot.
+    Ask only 1 question at a time.
 """
 def AI(user_input):
     completion = client.chat.completions.create(
@@ -29,26 +30,30 @@ def AI(user_input):
     )
     return completion.choices[0].message.content
 
-# user_input = input('User:')
-# print(AI(user_input))
+user_input = input('User:')
+print(AI(user_input))
+user_input = input('User:')
+print(AI(user_input))
+user_input = input('User:')
+print(AI(user_input))
 
 
 # website에 값 받고 출력하기
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('2.AI_friend.html')
+# @app.route('/')
+# def index():
+#     return render_template('2.AI_friend.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    user_input = request.form['user_input']
-    ai_response = AI(user_input)
-    return jsonify({'ai_response': ai_response})
+# @app.route('/process', methods=['POST'])
+# def process():
+#     user_input = request.form['user_input']
+#     ai_response = AI(user_input)
+#     return jsonify({'ai_response': ai_response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-    #user_input, AIOutput
-    #button id = sendData / textarea id = UserInput
+#     #user_input, AIOutput
+#     #button id = sendData / textarea id = UserInput
